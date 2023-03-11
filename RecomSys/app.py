66010f3ffcfb54c0ea6b0e1c0d4d3189d9d0fcad
MANTHIPERERA <manthi.perera@gmail.com>
2023-03-11 11:52:21 +0530

    recom Sys update

---
+++ RecomSys/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 11:49:19 2023
+
+@author: manthiperera
+"""
+
+from flask import Flask
+from views import views
+
+app = Flask (__name__)
+app.register_blueprint(views,url_prefix="/")
+
+if __name__== '__main__':
+    app.run(debug=True, port=8000)
+    
+    
---
+++ RecomSys/views.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, render_template
+from MovieLens import MovieLens
+from surprise import SVD
+
+views = Blueprint(__name__,"views")
+
+def BuildAntiTestSetForUser(testSubject, trainset):
+    fill = trainset.global_mean
+
+    anti_testset = []
+    
+    u = trainset.to_inner_uid(str(testSubject))
+    
+    user_items = set([j for (j, _) in trainset.ur[u]])
+    anti_testset += [(trainset.to_raw_uid(u), trainset.to_raw_iid(i), fill) for
+                             i in trainset.all_items() if
+                             i not in user_items]
+    return anti_testset
+
+@views.route("/")
+def home():
+    # Pick an arbitrary test subject
+    testSubject = 90
+
+    ml = MovieLens()
+
+    print("Loading movie ratings...")
+    data = ml.loadMovieLensLatestSmall()
+
+    userRatings = ml.getUserRatings(testSubject)
+    loved = []
+    hated = []
+    for ratings in userRatings:
+        if (float(ratings[1]) > 4.0):
+            loved.append(ratings)
+        if (float(ratings[1]) < 3.0):
+            hated.append(ratings)
+
+    print("\nUser ", testSubject, " loved these movies:")
+    for ratings in loved:
+        print(ml.getMovieName(ratings[0]))
+    print("\n...and didn't like these movies:")
+    for ratings in hated:
+        print(ml.getMovieName(ratings[0]))
+
+    print("\nBuilding recommendation model...")
+    trainSet = data.build_full_trainset()
+
+    algo = SVD()
+    algo.fit(trainSet)
+
+    print("Computing recommendations...")
+    testSet = BuildAntiTestSetForUser(testSubject, trainSet)
+    predictions = algo.test(testSet)
+
+    recommendations = []
+
+    print ("\nWe recommend:")
+    for userID, movieID, actualRating, estimatedRating, _ in predictions:
+        intMovieID = int(movieID)
+        recommendations.append((intMovieID, estimatedRating))
+
+    recommendations.sort(key=lambda x: x[1], reverse=True)
+
+    recommended_movies = [ml.getMovieName(ratings[0]) for ratings in recommendations[:10]]
+
+    return render_template("recommendations.html", recommended_movies=recommended_movies)
