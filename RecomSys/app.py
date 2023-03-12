483a23e0f4df0b3f5051db1b63ac9af11194c6b1
MANTHIPERERA <manthi.perera@gmail.com>
2023-03-12 18:25:48 +0530

    python recom Sys updated

--- RecomSys/app.py
+++ RecomSys/app.py
@@ -6,13 +6,11 @@ Created on Mon Mar  6 11:49:19 2023
 @author: manthiperera
 """
 
-from flask import Flask
+from flask import Flask #Creates flask Application
 from views import views
 
-app = Flask (__name__)
-app.register_blueprint(views,url_prefix="/")
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/")
 
-if __name__== '__main__':
-    app.run(debug=True, port=8000)
-    
-    
+if __name__ == '__main__':
+    app.run(debug=True)
