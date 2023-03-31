a3daa785fa5b565cd2dc3f3640b8109a1ddfc6c3
kalpanakanagasabai <kanagasabaikalpana3@gmail.com>
2023-03-31 22:33:40 +0530

    Quiz for ol is committed and pushed

---
+++ OL_Biology_quiz/js/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a function of the liver?",
+    answer: "Secretion of insulin",
+    options: [
+      "Detoxification of drugs and toxins",
+      "Production of bile",
+      "Storage of glucose as glycogen",
+      "Secretion of insulin"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a characteristic of prokaryotic cells? ",
+    answer: "They contain membrane-bound organelles",
+    options: [
+      "They lack a nucleus",
+      "They have a cell wall ",
+      "They contain membrane-bound organelles",
+      "They contain membrane-bound organelles "
+    ]
+  },
+    
+    {
+    numb: 3,
+    question: "Which of the following is not a type of RNA? ",
+    answer: "DNA",
+    options: [
+      "mRNA",
+      "tRNA",
+      "rRNA",
+      "DNA"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a function of the skeletal system?",
+    answer: "Production of red blood cells",
+    options: [
+      "Protection of vital organs",
+      "Movement of the body",
+      "Production of red blood cells",
+      "Storage of minerals such as calcium and phosphorus"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not a type of passive transport?",
+    answer: "Active transport",
+    options: [
+      " Diffusion",
+      "Osmosis",
+      "Facilitated diffusion",
+      "Active transport"
+    ]
+  },
+
+];
