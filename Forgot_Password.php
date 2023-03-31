d5914fbebefc8f9baa057ae63d5cef1d9beddcdd
SasmikaDevmith894 <sasmika.20200894@iit.ac.lk>
2023-04-01 01:26:51 +0530

    Passwod Reset in PHP

---
+++ Forgot_Password.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_GET["token"])) {
+    $token = $_GET["token"];
+
+    // Verify token exists in database
+    $stmt = $conn->prepare("SELECT * FROM password_resets WHERE token = ?");
+    $stmt->bind_param("s", $token);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Verify token exists in database
+    $stmt = $conn->prepare("SELECT * FROM password_resets WHERE token = ?");
+    $stmt->bind_param("s", $token);
+    $stmt->execute();
+    $result = $stmt->get_result();
+}
