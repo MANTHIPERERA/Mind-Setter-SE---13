2429263af8bbef9076c362c2c571a3181eb73170
kalpanakanagasabai <kanagasabaikalpana3@gmail.com>
2023-03-31 21:44:20 +0530

    Quiz for O/L English is committed and pushed

---
+++ OL_English_quiz/js/questions.js
@@ -0,0 +1,97 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Choose the correct form of the verb to complete the sentence: She _____ a book at the library yesterday.",
+    answer: "borrowed",
+    options: [
+      " borrow",
+      "borrowed",
+      "borrowing",
+      "borrows"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Choose the correctly punctuated sentence:",
+    answer: "Sarah wants to go to the movies.",
+    options: [
+      "Sarah, wants to go to the movies.",
+      " Sarah wants to go to the movies.",
+      "Sarah wants, to go to the movies.",
+      "Sarah wants to, go to the movies."
+    ]
+  },
+    
+    {
+    numb: 3,
+    question: "Choose the word that best completes the sentence: The cat sat _____ the windowsill.",
+    answer: "on",
+   
+    options: [
+      "in",
+      "on",
+      "at",
+      "under"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Choose the correctly spelled word:",
+    answer: "occurrence",
+    options: [
+      "occurence",
+      "occurrance",
+      "occurrence",
+      "occuranse"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Choose the sentence with the correct subject-verb agreement:",
+    answer: "The dog barks loudly",
+    options: [
+      " The dogs barks loudly.",
+      "The dog barks loudly.",
+      "The dogs bark loudly.",
+      "The dog bark loudly."
+    ]
+  },
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
