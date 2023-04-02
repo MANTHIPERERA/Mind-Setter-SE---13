9af4dba400534b0158c749674beddefdfac24ed0
Kumod Hansidu Munasingha Arachchi <w1867092@my.westminster.ac.uk>
2023-04-02 10:06:01 +0530

    adaptive test

---
+++ component/grades.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="container-xl p-5 ">
+        <div class="textH" style="font-size:4vw;">Grades we teach<br><br></div>
+
+
+        <div class="d-grid gap-1" style="position: relative;left: 130px;">
+            <div class="button1 ">
+                <button class="btn1 btn-primary btn-lg m-2  " type="button">Grade - 10</button>
+                <button class="btn1 btn-primary btn-lg  " type="button">Grade - 11</button>
+            </div>
+            <div class="button1">
+                <button class="btn1 btn-primary btn-lg m-2 " type="button">Grade - 12</button>
+                <button class="btn1 btn-primary btn-lg " type="button">Grade - 13</button>
+            </div>
+        </div>
+    </div>
+</div>
