ac9aef659dd2c12feebd829cd4756585346d9ee7
MMCprasad <chirantha.20210210@iit.ac.lk>
2023-04-05 12:51:09 +0530

    howphp

--- component/howWork.php
+++ component/howWork.php
@@ -1,47 +1,37 @@
 <div class="row">
 
-    <div class="container-xxl">
-        <div class="text1 p-5" style="font-size:4vw;">
-            How our website works
-        </div>
+    <div class="container-xxl p-5">
+        <h3 style="font-size: 4.5vw;">How our website works</h3>
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                        aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                        aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                        aria-label="Slide 3"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
 
-            <div class="carousel-inner" style="text-align: center; padding: 30px;">
-                <div class="carousel-item active">
-                    <img src="Assets/SliderImage1.png" class="testimonial-image" alt="Slider image 1" height="ms-auto"
-                         width="60%">
-                </div>
+            <div class="carousel-inner" style = "text-align: center; padding: 30px;">
+              <div class="carousel-item active">
+                <img src="Assets/SliderImage1.png"  class = "testimonial-image" alt="Slider image 1" height = "ms-auto" width="60%">
+              </div>
+             
+              <div class="carousel-item">
+                <img src="Assets/SliderImage2.png" class = "testimonial-image" alt="Slider Image 2" height = "ms-auto"width="60%">
+              </div>
 
-                <div class="carousel-item">
-                    <img src="Assets/SliderImage2.png" class="testimonial-image" alt="Slider Image 2" height="ms-auto"
-                         width="60%">
-                </div>
-
-                <div class="carousel-item">
-                    <img src="Assets/SliderImage3.png" class="testimonial-image" alt="Slider Image 3" height="ms-auto"
-                         width="60%">
-                </div>
+              <div class="carousel-item">
+                <img src="Assets/SliderImage3.png" class = "testimonial-image" alt="Slider Image 3" height = "ms-auto" width="60%">
+              </div>
 
             </div>
 
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
             </button>
 
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
             </button>
 
         </div>
